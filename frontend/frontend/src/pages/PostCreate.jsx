f906a3add1a8998e87243723f81c3bb138f73633
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-04-22 16:42:50 +0530

    added all the neseceraay steps

--- frontend/frontend/src/pages/PostCreate.jsx
+++ frontend/frontend/src/pages/PostCreate.jsx
@@ -1,12 +1,33 @@
+import '../css/PostCreate.css'
+import { generatePost } from '../services/api';
+import { useNavigate  , Link} from 'react-router-dom';
+
 export default function PostCreate(){
+         const navigate = useNavigate();   
+        const handleSubmit=async (e)=>{
+                e.preventDefault(); //stops the page to get reloaded
+                //console.log(e.target.image.files)
+                const formData = new FormData(e.target); //FormData(e.target) = automatically reads all inputs inside the form
+                await generatePost(formData);
+                 navigate('/post');
+        }
+
     return (
         <section className="post-create">
                 <h1>Create Post!</h1>
+                <nav className='navbar-post-crt'>
+                    <div className='navbar-link'>
+                        <Link to='/post'>
+                            <img src="https://imgs.search.brave.com/4Nczsa9Xtpx2Q9TICs_W9tXNYBl-8tV6k4Fk2GySmp4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbnNjb3V0LmNv/bS9pY29uL3ByZW1p/dW0vcG5nLTI1Ni10/aHVtYi9iYWNrLWJ1/dHRvbi1pY29uLXN2/Zy1kb3dubG9hZC1w/bmctMzc1NzM1MS5w/bmc_Zj13ZWJwJnc9/MTI4" alt="back button" />
+                        </Link>
 
-                <form >
-                    <input type="file" name="image" accept="image/*" />
-                    <input type="text" name="caption" required />
-                    <button className="gen-post-btn">Generate Post</button>
+                    </div>
+                </nav>
+                <form onSubmit={handleSubmit} className="post-create-form">
+                    
+                    <input className="post-create-input" type="file" name="image" accept="image/*" />
+                    <input className="post-create-input" type="text" name="caption" placeholder="Enter the caption" required />
+                    <button className="gen-post-btn" type="submit">Generate Post</button>
 
                 </form>
         </section>
--- frontend/frontend/src/pages/Posts.jsx
+++ frontend/frontend/src/pages/Posts.jsx
@@ -1,7 +1,49 @@
-export default function Posts(){
-    return (
-      <section className="Posts">
+import { useEffect, useState } from "react"
+import { getPost } from "../services/api"
+import '../css/Posts.css'
+import { Link } from "react-router-dom"
 
+
+export default function Posts(){
+    const [posts , setPosts] = useState([]) //empty array to store the intial images 
+
+
+    useEffect(()=>{
+
+  const fetchData =async ()=>{ 
+      const data = await getPost();
+      console.log(data); //here we are getting the data from the server and the db of posts we have created
+      setPosts(data);
+  } 
+
+  fetchData();//runnig so that it will fetch the data intially
+},[])
+
+  return (
+      <section className="Posts-section">
+          <nav className="navbar">
+            <div className="posts-title">
+                <Link to="/post">Post App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/post-create" className="nav-link">
+                    <img src="https://imgs.search.brave.com/AzX08iQmGwpjCSx5sBlKQd9_AXtD7tnvwpf7rkaqxgg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvcHJldmll/dy0xeC85Ny82NC9w/bHVzLWljb24tdmVj/dG9yLTEwOTE5NzY0/LmpwZw" alt="" />
+                </Link>
+            </div>
+    </nav>
+          <div className="posts">
+            {
+              posts.map((p)=>{
+                return (
+                <div className="post-card" key={p._id}>
+                    <img src={p.image} alt={p.caption} />
+                    <h3 className="post-caption">{p.caption}</h3>
+
+                </div>
+                )
+              })
+            }
+          </div>
       </section>
     )
 }
